c7ff0af25ca247b4fdc2a9adf741c2ab6667bbe4
Vivek M. Chawla <Vivek.M.Chawla@gmail.com>
2026-02-12 13:42:22 -0800

    Update copyright information and license details across multiple scripts to Apache-2.0

--- scripts/js/sfdx-falcon/task-runner/sfdx-task.mjs
+++ scripts/js/sfdx-falcon/task-runner/sfdx-task.mjs
@@ -1,12 +1,27 @@
+/*
+ * Copyright (c) 2026, Salesforce, Inc.
+ * SPDX-License-Identifier: Apache-2
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 //─────────────────────────────────────────────────────────────────────────────────────────────────┐
 /**
  * @file          sfdx-falcon/task-runner/sfdx-task.mjs
- * @copyright     Vivek M. Chawla - 2023
- * @author        Vivek M. Chawla <@VivekMChawla>
+ * @author        Vivek M. Chawla <@VivekMChawla> (original 2023)
  * @summary       Wraps a `Listr` task with helpful tools to work with Salesforce CLI commands.
  * @description   Makes it easier to work with `Listr` tasks when automating Salesforce CLI commands.
  * @version       1.0.0
- * @license       BSD-3-Clause
+ * @license       Apache-2.0
  */
 //─────────────────────────────────────────────────────────────────────────────────────────────────┘
 // Import External Libraries & Modules
